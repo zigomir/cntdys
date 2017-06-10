e469a8272bf53c4cf35a25ed9f132ce9cc71192a
zigomir <zigomir@gmail.com>
2017-06-10 13:57:30 -0700

    Comment out things not in use.

--- src/index.ts
+++ src/index.ts
@@ -1,53 +1,39 @@
-interface Month {
-  name: MonthName
-  number: MonthNumber
-  year: number
-  days: 28 | 29 | 30 | 31
-}
+// interface Month {
+//   name: MonthName
+//   number: MonthNumber
+//   year: number
+//   days: 28 | 29 | 30 | 31
+// }
 
-interface MonthCalendar {
-  month: Month
-}
+// interface MonthCalendar {
+//   month: Month
+// }
 
-interface YearCalendar {
-  year: Year
-}
+// interface YearCalendar {
+//   year: Year
+// }
 
-type DayName = 'Monday' | 'Tuesday' | 'Wednesday' | 'Thursday' | 'Friday' | 'Saturday' | 'Sunday'
+// type DayName = 'Monday' | 'Tuesday' | 'Wednesday' | 'Thursday' | 'Friday' | 'Saturday' | 'Sunday'
 
+// type MonthName =
+//   | 'January'
+//   | 'February'
+//   | 'March'
+//   | 'April'
+//   | 'May'
+//   | 'June'
+//   | 'July'
+//   | 'August'
+//   | 'September'
+//   | 'October'
+//   | 'November'
+//   | 'December'
+
+// type Year = [Month, Month, Month, Month, Month, Month, Month, Month, Month, Month, Month, Month]
 type MonthNumber = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12
 
-type MonthName =
-  | 'January'
-  | 'February'
-  | 'March'
-  | 'April'
-  | 'May'
-  | 'June'
-  | 'July'
-  | 'August'
-  | 'September'
-  | 'October'
-  | 'November'
-  | 'December'
-
-type Year = [Month, Month, Month, Month, Month, Month, Month, Month, Month, Month, Month, Month]
-
 export function getDaysInMonth(year: number, month: MonthNumber) {
-  const daysInMonth = [
-    31, // jan
-    [28, 29], // feb
-    31, // mar
-    30, // apr
-    31, // may
-    30, // jun
-    31, // jul
-    31, // aug
-    30, // sep
-    31, // oct
-    30, // nov
-    31 // dec
-  ]
+  const daysInMonth = [31, [28, 29], 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
   // naive leap year implementation
   if (month === 2) {
