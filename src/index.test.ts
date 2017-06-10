827b4aedfde7ff693bb529a2a4d5da08d1cb61db
zigomir <zigomir@gmail.com>
2017-06-10 13:54:38 -0700

    Slice by weeks.

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,8 +1,18 @@
-import { getDaysInMonth } from './index'
+import { getDaysInMonth, listDays } from './index'
 
 test('get days in month', () => {
   expect(getDaysInMonth(2016, 2)).toBe(29)
   expect(getDaysInMonth(2017, 2)).toBe(28)
   expect(getDaysInMonth(2017, 1)).toBe(31)
   expect(getDaysInMonth(2017, 8)).toBe(31)
+})
+
+test('prints out whole month', () => {
+  expect(listDays(2016, 2)).toEqual([
+    [1, 2, 3, 4, 5, 6, 7],
+    [8, 9, 10, 11, 12, 13, 14],
+    [15, 16, 17, 18, 19, 20, 21],
+    [22, 23, 24, 25, 26, 27, 28],
+    [29]
+  ])
 })
--- src/index.ts
+++ src/index.ts
@@ -56,3 +56,18 @@ export function getDaysInMonth(year: number, month: MonthNumber) {
 
   return daysInMonth[month - 1]
 }
+
+export function listDays(year: number, month: MonthNumber) {
+  const daysInMonth = getDaysInMonth(year, month)
+  const allDays = [...Array(daysInMonth).keys()].map(d => d + 1)
+
+  const weeks = [
+    allDays.slice(0, 7),
+    allDays.slice(7, 14),
+    allDays.slice(14, 21),
+    allDays.slice(21, 28),
+    allDays.slice(28, 31)
+  ]
+
+  return weeks
+}
